2062d54352d55e4d2e24dc2d19d3206714b5333d
bbqboneless <sammylicea5@gmail.com>
2024-01-05 11:39:22 -0600

    First Program in Java

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    //public = access modifier
+    //static = run inside a static context, not instantiate it as an object
+    //String[] args = arguments for our main method (array of strings called args)
+    public static void main(String[] args){
+        //Statement
+        System.out.println("Hello World!");
+    }
+}
