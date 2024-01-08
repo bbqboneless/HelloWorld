b10e62fa3160bb43df1e346cc2bd84a545e5de7c
bbqboneless <sammylicea5@gmail.com>
2024-01-08 09:39:26 -0600

    Char and Boolean PT

---
+++ src/CharExamples.java
@@ -0,0 +1,17 @@
+public class CharExamples {
+    public static void main(String[] args) {
+        //single quotes
+        //Java primitive char supports 16 bit Unicode (Basic)
+        char myChar = 'x';
+        System.out.println(myChar);
+
+        //JAVA UNICODE
+        char myUnicode = '\u018A';
+        System.out.println(myUnicode);
+
+        //Boolean
+        boolean isWeekday = true;
+        boolean isWarm = false;
+        System.out.println("Is Weekday? " + isWeekday + ", Is warm? " + isWarm);
+    }
+}
